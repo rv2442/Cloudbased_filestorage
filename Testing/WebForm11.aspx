23974d85c38e8989b0fdab5ab073e15e00fc2cc8
VineetDabholkar2002 <93699671+VineetDabholkar2002@users.noreply.github.com>
2021-12-16 21:47:30 +0530

    Add files via upload

---
+++ Testing/WebForm11.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm11.aspx.cs" Inherits="LoginPass.WebForm11" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+<asp:FileUpload ID="FileUpload1" runat="server" />
+<asp:Button ID="btnUpload" runat="server" Text="Submit" onclick="btnUpload_Click"  />
+<hr />
+<asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False"
+            EmptyDataText = "No files uploaded" CellPadding="4"
+            EnableModelValidation="True" ForeColor="#333333" GridLines="None">
+    <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+    <Columns>
+        <asp:BoundField DataField="Text" HeaderText="File Name" />
+        <asp:TemplateField>
+            <ItemTemplate>
+                <asp:LinkButton ID="lnkDownload" Text = "Download" CommandArgument = '<%# Eval("Value") %>' runat="server" OnClick = "DownloadFile"></asp:LinkButton>
+            </ItemTemplate>
+        </asp:TemplateField>
+        <asp:TemplateField>
+            <ItemTemplate>
+                <asp:LinkButton ID = "lnkDelete" Text = "Delete" CommandArgument = '<%# Eval("Value") %>' runat = "server" OnClick = "DeleteFile" />
+            </ItemTemplate>
+        </asp:TemplateField>
+    </Columns>
+    <EditRowStyle BackColor="#999999" />
+    <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+    <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+    <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+    <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+    <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+</asp:GridView>
+        
+    </form>
+</body>
+</html>
---
+++ Testing/WebForm11.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.IO;
+
+namespace LoginPass
+{
+    public partial class WebForm11 : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindGrid();
+            }
+        }
+        protected void BindGrid()
+        {
+            string[] filePaths = Directory.GetFiles(Server.MapPath("~/MyUploads/"));
+            List<ListItem> files = new List<ListItem>();
+            foreach (string filePath in filePaths)
+            {
+                files.Add(new ListItem(Path.GetFileName(filePath), filePath));
+            }
+            GridView1.DataSource = files;
+            GridView1.DataBind();
+        }
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+            {
+                FileUpload1.SaveAs(Server.MapPath("~/MyUploads/") + FileUpload1.FileName);
+                BindGrid();
+            }
+            else
+            {
+                Response.Write("Please select file to upload");
+            }
+
+        }
+        protected void DownloadFile(object sender, EventArgs e)
+        {
+            string filePath = (sender as LinkButton).CommandArgument;
+            Response.ContentType = ContentType;
+            Response.AppendHeader("Content-Disposition", "attachment; filename=" + Path.GetFileName(filePath));
+            Response.WriteFile(filePath);
+            Response.End();
+        }
+        protected void DeleteFile(object sender, EventArgs e)
+        {
+            string filePath = (sender as LinkButton).CommandArgument;
+            File.Delete(filePath);
+            BindGrid();
+        }
+    }
+}
