568cbeaba02756f3b14814a4bd287d3934b8c5da
Rahul Vijan <69571769+rv2442@users.noreply.github.com>
2021-12-26 15:27:36 +0530

    Add files via upload

---
+++ Testing/tastpaithan.aspx
@@ -0,0 +1,82 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="tastpaithan.aspx.cs" Inherits="ironpaithantest.tastpaithan" %>
+
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+       <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+       <script src="Webcam.js" type="text/javascript"></script>
+       <script type="text/javascript">
+
+           $(function () {
+               Webcam.set({
+                   width: 1080,
+                   height: 606,
+                   image_format: 'jpg',
+                   jpg_quality: 90
+               });
+               Webcam.attach('#idwebcam');
+
+               $("#btncapture").click(function () {
+                   Webcam.snap(function (data_url) {
+                       $("#idcaptured")[0].src = data_url;
+                   });
+               })
+           });
+      
+       </script>
+    <script type="text/javascript">
+        function getBase64Image(img) {
+            var canvas = document.createElement("canvas");
+            canvas.width = img.width;
+            canvas.height = img.height;
+            var ctx = canvas.getContext("2d");
+            ctx.drawImage(img, 0, 0);
+           
+            var dataURL = canvas.toDataURL("image/png");
+            return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+        }
+
+        function abcd() {
+            var base64 = getBase64Image(document.getElementById("idcaptured"));
+            console.log(base64);
+            document.getElementById("hidden_img").value = base64;
+            document.getElementById('<%= hiddenbtn.ClientID %>').click();
+
+        }
+    </script>
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:HiddenField ID="hidden_img" runat="server" />
+        <asp:Button ID="hiddenbtn" runat="server" Text="Button" OnClick="hiddenbtn_Click"  style="display:none;" />
+        <table border="0" cellpadding="0" cellspacing="0">
+
+        <tr>
+            <th align="center">Live Cam</th>
+            <th align="center">Pic Captured</th>
+        </tr>
+            <tr>
+                <td>
+        <div id="idwebcam" >
+        </div>
+                    </td>
+                <td>
+        <img id="idcaptured" />
+                    </td>
+                </tr>
+            <tr>
+                <td>
+        <input  type="button" id="btncapture" value="capture" />
+                    </td>
+                <td>
+                    <input type="button" id="abcdbutton" value="console" onclick="abcd()" />
+                </td>
+                </tr>
+            </table>
+    </form>
+    </body>
+</html>
---
+++ Testing/tastpaithan.aspx.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using IronPython.Hosting;
+using IronPython.Runtime;
+using IronPython;
+using Microsoft.Scripting.Hosting;
+using Microsoft.Scripting;
+using System.IO;
+using System.Diagnostics;
+using System.Text;
+
+namespace ironpaithantest
+{
+    public partial class tastpaithan : System.Web.UI.Page
+    {
+       
+
+            protected void Page_Load(object sender, EventArgs e)
+            {
+
+            }
+
+        protected void hiddenbtn_Click(object sender, EventArgs e)
+        {
+            var ab= hidden_img.Value;
+            Response.Write(ab);
+            string filePath = "D:\\face_rec\\face_rec\\test.jpg";
+            File.WriteAllBytes(filePath, Convert.FromBase64String(ab));
+
+            //System.Diagnostics.Process process = new System.Diagnostics.Process();
+            //System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            //startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            //startInfo.FileName = "cmd.exe";
+            //startInfo.Arguments = "/C copy /b Image1.jpg + Archive.rar Image2.jpg";
+            //process.StartInfo = startInfo;
+            //process.Start();
+
+            //string strCmdText;
+            //strCmdText = "/K cd /d D:\\face_rec\\face_rec && python face_rec.py ";
+            //System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+            try
+            {
+                var p = new Process();
+                p.StartInfo.FileName = "cmd.exe";
+                p.StartInfo.Arguments = @"/k cd /d D:\\face_rec\\face_rec && python face_rec.py ";
+                p.StartInfo.CreateNoWindow = true;
+                p.StartInfo.RedirectStandardError = true;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.RedirectStandardInput = false;
+                p.StartInfo.UseShellExecute = false;
+                var stdOutput = new StringBuilder();
+                var stderror = new StringBuilder();
+                p.OutputDataReceived += (a, b) => stdOutput.AppendLine(b.Data);
+                p.ErrorDataReceived += (a, b) => stderror.AppendLine(b.Data);
+                p.Start();
+                p.BeginErrorReadLine();
+                p.BeginOutputReadLine();
+                p.WaitForExit(15000);
+                p.Kill();
+                Response.Write("<br><br> data: "+stdOutput.ToString());
+                Response.Write("<br><br> error: "+stderror.ToString());
+            }
+            catch
+            {
+                Response.Write("<br><br> error");
+            }
+
+
+            //string fileName = @"D:\face_rec\face_rec\face_rec.py";
+
+            //Process p = new Process();
+            //p.StartInfo = new ProcessStartInfo(@"C:\Users\Rahul\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7", fileName)
+            //{
+            //    RedirectStandardOutput = true,
+            //    UseShellExecute = false,
+            //    CreateNoWindow = true
+            //};
+            //p.Start();
+
+            //string output = p.StandardOutput.ReadToEnd();
+            //p.WaitForExit();
+
+            //Response.Write(output);
+
+            //Console.ReadLine();
+            // Response.Write("Value received from script: " + myString);
+        }
+    }
+}
