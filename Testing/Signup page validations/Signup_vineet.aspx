e724b07c0c60e454a6f00e0a9b3f0ebe9e49df4a
Rahul Vijan <69571769+rv2442@users.noreply.github.com>
2021-12-28 20:07:45 +0530

    Add files via upload

---
+++ Testing/Signup page validations/Signup_vineet.aspx
@@ -0,0 +1,175 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Signup_vineet.aspx.cs" Inherits="Website_.NET.Signup_vineet" %>
+<!DOCTYPE html>
+<script runat="server">
+</script>
+
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style3 {
+            width: 954px;
+            height: 433px;
+            position: absolute;
+            top: 69px;
+            left: 395px;
+            z-index: 1;
+            text-align: left;
+        }
+        .auto-style4 {
+            color: #FF0000;
+        }
+        .auto-style5 {
+            width: 455px;
+            height: 20px;
+            position: absolute;
+            top: 100px;
+            left: 353px;
+            z-index: 1;
+        }
+        .auto-style6 {
+            position: absolute;
+            top: 158px;
+            left: 341px;
+            z-index: 1;
+            width: 461px;
+        }
+        .auto-style7 {
+            position: absolute;
+            top: 63px;
+            left: 393px;
+            z-index: 1;
+            margin-bottom: 0px;
+        }
+        .auto-style8 {
+            position: absolute;
+            top: 43px;
+            left: 317px;
+            z-index: 1;
+            width: 23px;
+            height: 17px;
+        }
+        .auto-style9 {
+            position: absolute;
+            top: 91px;
+            left: 319px;
+            z-index: 1;
+            width: 25px;
+            height: 17px;
+        }
+        .auto-style10 {
+            position: absolute;
+            top: 166px;
+            left: 320px;
+            z-index: 1;
+            width: 30px;
+            height: 19px;
+        }
+    </style>
+</head>
+<body>
+    <script type="text/javascript">
+        function nextpage() {
+            window.location.href = "gmail_otp.aspx";
+        }
+
+        function downloadFile(data = "<%=Session["scrkey"].ToString()%>", fileName = "<%=txtusername.Text%>",type = "text/plain") {
+          
+        var validation = "<%=Session["validity"].ToString()%>";
+        //window.alert(validation);
+
+            if (validation=="True") {
+                // Create an invisible A element
+                const a = document.createElement("a");
+                a.style.display = "none";
+                document.body.appendChild(a);
+
+                // Set the HREF to a Blob representation of the data to be downloaded
+                a.href = window.URL.createObjectURL(
+                    new Blob([data], { type })
+                );
+
+                // Use download attribute to set set desired file name
+                a.setAttribute("download", fileName);
+
+                // Trigger the download by simulating click
+                a.click();
+
+                // Cleanup
+                window.URL.revokeObjectURL(a.href);
+                document.body.removeChild(a);
+
+                nextpage();
+            }
+            else {
+                //window.prompt(validation);
+            }
+            console.log(validation);
+        }
+        downloadFile(data = "<%=Session["scrkey"].ToString()%>", fileName = "<%=txtusername.Text%>", type = "text/plain");
+    </script>
+    <form id="form1" runat="server">
+            <asp:Button ID="Button2" runat="server" Text="Button" style="visibility:hidden;" OnClick="Button2_Click"  />
+
+    <div>
+    
+    </div>
+        <p>
+            &nbsp;&nbsp;
+        </p>
+        <p>
+            &nbsp;</p>
+        <asp:Panel ID="Panel1" runat="server" CssClass="auto-style3">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Sign Up<br />
+            <asp:Label ID="uname_label" runat="server" style="z-index: 1; left: 326px; top: 55px; position: absolute; height: 24px; width: 241px" ViewStateMode="Enabled"></asp:Label>
+            <br />
+            <br />
+            Enter Username:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="txtusername" runat="server" Height="16px" OnTextChanged="txtusername_TextChanged" Width="181px" AutoPostBack="True"></asp:TextBox>
+            <asp:RegularExpressionValidator ID="regusername" runat="server" ControlToValidate="txtusername" CssClass="auto-style7" Display="Dynamic" ErrorMessage="Invalid Username" ForeColor="Red" SetFocusOnError="True" ValidationExpression="^[a-zA-Z0-9]+"></asp:RegularExpressionValidator>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="txtusername" CssClass="auto-style8" Display="Dynamic" ErrorMessage="*" ForeColor="Red" SetFocusOnError="True"></asp:RequiredFieldValidator>
+            <br />
+            <br />
+            Enter Email :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="txtpassword" runat="server" TextMode="Password" Width="184px" style="z-index: 1; left: 128px; top: 138px; position: absolute"></asp:TextBox>
+            &nbsp;<asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ControlToValidate="txtpassword" CssClass="auto-style9" ErrorMessage="*" ForeColor="Red" SetFocusOnError="True"></asp:RequiredFieldValidator>
+&nbsp;<asp:Label ID="Label1" runat="server" CssClass="auto-style6" ForeColor="Red"></asp:Label>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <br />
+            <br />
+            Enter Password :&nbsp;
+            <asp:TextBox ID="txtconfpass" runat="server" TextMode="Password" Width="183px" style="z-index: 1; position: absolute; top: 176px; left: 129px;"></asp:TextBox>
+            &nbsp;<asp:Label ID="lblmsg" runat="server" CssClass="auto-style5" ForeColor="Red"></asp:Label>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <br />
+            <br />
+            Confirm Password:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="txtemail" runat="server" TextMode="Email" Width="184px" style="z-index: 1; left: 128px; top: 101px; position: absolute" AutoPostBack="True" OnTextChanged="txtemail_TextChanged"></asp:TextBox>
+            <asp:Label ID="email_label" runat="server" AssociatedControlID="txtemail" style="z-index: 1; left: 327px; top: 104px; position: absolute; width: 170px" Text="Label"></asp:Label>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server" ControlToValidate="txtemail" CssClass="auto-style10" Display="Dynamic" ErrorMessage=" *" ForeColor="Red"></asp:RequiredFieldValidator>
+            <br />
+            <br />
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button1" runat="server" Font-Bold="True" OnClick="Button1_Click"  onclientclick="downloadFile()" Text="Register" />
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator4" runat="server" ControlToValidate="txtpassword" ErrorMessage="*" ForeColor="Red" style="z-index: 1; left: 316px; top: 126px; position: absolute; height: 16px; width: 14px"></asp:RequiredFieldValidator>
+        </asp:Panel>
+        <p>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span class="auto-style4">&nbsp;</span></p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
+
+
---
+++ Testing/Signup page validations/Signup_vineet.aspx.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace Website_.NET
+{
+    public partial class Signup_vineet : System.Web.UI.Page
+    {
+        string finalString;
+        public string scrkey;
+        public string usrname;
+        public string username;
+        static bool email_unique;
+        static bool username_unique;
+        public bool allvalidationpassed;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                email_unique = false;
+                username_unique = false;
+                SecretKey();
+                Session["validity"] = "initial";
+
+
+            }
+                this.scrkey = finalString;
+                this.usrname = username;
+            Response.Write(Session["validity"].ToString());
+        }
+        public void SecretKey()
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[8];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            finalString = new String(stringChars);
+            Session["scrkey"] = finalString;
+        }
+        
+        public bool ValidatePassword(string password, out string ErrorMessage)
+        {
+            var input = password;
+            ErrorMessage = string.Empty;
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                //Response.Write("<script>alert('password cant be blank');</script>");
+            }
+
+            var hasNumber = new Regex(@"[0-9]+");
+            var hasUpperChar = new Regex(@"[A-Z]+");
+            var hasMiniMaxChars = new Regex(@".{8,15}");
+            var hasLowerChar = new Regex(@"[a-z]+");
+            var hasSymbols = new Regex(@"[!@#$%^&*()_+=[{\]};:<>|./?,-]");
+
+            if (!hasLowerChar.IsMatch(input))
+            {
+                ErrorMessage = "Password should contain at least one lower case letter.";
+                allvalidationpassed = false;
+                Session["validity"] = allvalidationpassed.ToString();
+                return false;
+            }
+            else if (!hasUpperChar.IsMatch(input))
+            {
+                ErrorMessage = "Password should contain at least one upper case letter.";
+                allvalidationpassed = false;
+                Session["validity"] = allvalidationpassed.ToString();
+                return false;
+            }
+            else if (!hasMiniMaxChars.IsMatch(input))
+            {
+                ErrorMessage = "Password should not be lesser than 8 or greater than 15 characters.";
+                allvalidationpassed = false;
+                Session["validity"] = allvalidationpassed.ToString();
+                return false;
+            }
+            else if (!hasNumber.IsMatch(input))
+            {
+                ErrorMessage = "Password should contain at least one numeric value.";
+                allvalidationpassed = false;
+                Session["validity"] = allvalidationpassed.ToString();
+                return false;
+            }
+
+            else if (!hasSymbols.IsMatch(input))
+            {
+                ErrorMessage = "Password should contain at least one special case character.";
+                allvalidationpassed = false;
+                Session["validity"] = allvalidationpassed.ToString();
+                return false;
+            }
+            else if (txtpassword.Text != txtconfpass.Text)
+            {
+                lblmsg.Visible = true;
+                lblmsg.Text = "Password and Confirm Password should match";
+                allvalidationpassed = false;
+                Session["validity"] = allvalidationpassed.ToString();
+                
+
+                txtconfpass.Text = "";
+                txtconfpass.Focus();
+                return false;
+            }
+            else if (email_unique == false)
+            {
+                email_label.Text = "Email not valid";
+                txtemail.Focus();
+                allvalidationpassed = false;
+                Session["validity"] = allvalidationpassed.ToString();
+                return false;
+            }
+            else if (username_unique == false)
+            {
+                uname_label.Text = "Username has already been taken";
+                txtusername.Focus();
+                allvalidationpassed = false;
+                Session["validity"] = allvalidationpassed.ToString();
+                return false;
+            }
+            else
+            {
+                allvalidationpassed = true;
+                Session["validity"] = allvalidationpassed.ToString();
+                return true;
+            }
+        }
+
+        
+        protected void Button1_Click(object sender, EventArgs e)
+        {  
+            bool pass = ValidatePassword(txtpassword.Text, out string y);
+            Session["validity"] = pass.ToString();
+            Label1.Text = y;
+            if (pass)
+            {
+                //if (txtpassword.Text != txtconfpass.Text)
+                //{
+                //    lblmsg.Visible = true;
+                //    lblmsg.Text = "Password and Confirm Password should match";
+
+                //    txtconfpass.Text = "";
+                //    txtconfpass.Focus();
+                //}
+                //else if (email_unique == false)
+                //{
+                //    email_label.Text = "Email not valid";
+                //    txtemail.Focus();
+                //}
+                //else if (username_unique == false)
+                //{
+                //    uname_label.Text = "Username has already been taken";
+                //    txtusername.Focus();
+                //}
+                //else
+                //{
+
+                    SqlConnection con = new SqlConnection("Server=199.79.62.22;uid=training;pwd=Training@786;database=cmp");
+                    SqlCommand cmd = new SqlCommand("insert into cloudlogin values(@username,@password,@email,@secretkey)", con);
+                    string skey = Session["scrkey"].ToString();
+                    cmd.Parameters.AddWithValue("@username", txtusername.Text);
+                    cmd.Parameters.AddWithValue("@password", txtpassword.Text);
+                    cmd.Parameters.AddWithValue("@email", txtemail.Text);
+                    cmd.Parameters.AddWithValue("@secretkey", skey);
+                    username = txtusername.Text;
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    //TextFile(username, skey);
+                    //cmd.ExecuteNonQuery();
+                    con.Close();
+                   // Response.Redirect("gmail_otp.aspx");
+                //}
+            }
+            else
+            {
+
+            }
+        }
+
+        public void txtusername_TextChanged(object sender, EventArgs e)
+        {
+            string a="";
+            SqlConnection con = new SqlConnection("Server=199.79.62.22;uid=training;pwd=Training@786;database=cmp");
+            SqlCommand cmd = new SqlCommand("select * from cloudlogin where username = @username", con);
+            cmd.Parameters.AddWithValue("@username", txtusername.Text);
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+               a = dr[0].ToString();
+            }
+            if (dr.HasRows)
+            {
+                Response.Write(a);
+                uname_label.Text = "Username has already been taken";
+                txtusername.Focus();
+                username_unique = false;
+            }
+            else
+            {
+                uname_label.Text = "valid";
+                username_unique = true;
+                txtemail.Focus();
+
+            }
+            con.Close();
+        }
+
+        protected void txtemail_TextChanged(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Server=199.79.62.22;uid=training;pwd=Training@786;database=cmp");
+            SqlCommand cmd = new SqlCommand("select * from cloudlogin where email = @email", con);
+            cmd.Parameters.AddWithValue("@email", txtemail.Text);
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                email_label.Text = "Email not valid";
+                txtemail.Focus();
+                email_unique = false;
+            }
+            else
+            {
+                email_label.Text = "valid";
+                email_unique = true;
+                txtpassword.Focus();
+
+            }
+            con.Close();
+        }
+
+        protected void txtpassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Button2_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("gmail_otp.aspx");
+        }
+    }
+}
