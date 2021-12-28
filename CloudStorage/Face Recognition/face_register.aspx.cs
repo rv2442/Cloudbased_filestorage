6d1cddabf428cf68dccd9f7423acf15eef78d4de
VineetDabholkar2002 <93699671+VineetDabholkar2002@users.noreply.github.com>
2021-12-28 11:04:56 +0530

    Add files via upload

---
+++ CloudStorage/Face Recognition/face_register.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Website_.NET
+{
+    public partial class face_register : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Session["username"] = "Soul";
+        }
+        protected void hiddenbtn_Click(object sender, EventArgs e)
+        {
+            var ab = hidden_img.Value;
+            Response.Write(ab);
+            string filePath = "D:\\face_rec\\face_rec\\faces\\"+Session["username"].ToString()+".jpg";
+            File.WriteAllBytes(filePath, Convert.FromBase64String(ab));
+            Response.Redirect("Face_recognition.aspx");
+          
+
+
+        }
+    }
+}
