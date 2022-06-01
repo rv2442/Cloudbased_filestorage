64a955bbd5107f3d0c02219e6686b130b0c9d1de
VineetDabholkar2002 <93699671+VineetDabholkar2002@users.noreply.github.com>
2022-06-01 18:56:11 +0530

    Added comments

--- Scloud_Final/CloudStorage/CheckOTP.aspx.cs
+++ Scloud_Final/CloudStorage/CheckOTP.aspx.cs
@@ -1,3 +1,10 @@
+/*
+*    @author: Vineet Dabholkar, Rahul Vijan
+*
+*    This Page is used to Check OTP and create a database for the user where file information will be stored at
+*
+*/
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,16 +19,18 @@ namespace CloudStorage
 {
     public partial class WebForm3 : System.Web.UI.Page
     {
-        //Assign an empty string otp to store the OTP (One Time Password)
+        /* 
+        * Assign an empty string otp to store the OTP (One Time Password)
+        */
         string otp = "";
   
         protected void Page_Load(object sender, EventArgs e)
         {
             
-            //Session state is an ASP.NET Core scenario for storage of user data while the user browses a web app
+            //zSession state is an ASP.NET Core scenario for storage of user data while the user browses a web app
             if (Session["username"] != null)
             {
-                //Store session's OTP in string "otp" 
+                /* Store session's OTP in string "otp" */
                 otp = Session["OTP"].ToString();
             }
             else
@@ -30,36 +39,41 @@ namespace CloudStorage
             }
             if (!IsPostBack)
             {
-                //Send an alert when the OTP is sent to the Email ID provided by the user
+                /* Send an alert when the OTP is sent to the Email ID provided by the user */
                 ClientScript.RegisterStartupScript(GetType(), "alert", "alert('OTP sent to the given Email ID.');", true);
 
             }
         }
-
+        /* 
+        *   This function will check whether the OTP matches the users input or not
+        */
         protected void Button1_Click(object sender, EventArgs e)
         {
             if (otp == txtOTP.Text)
             {
-                //if the OTP entered by the user in the textbox matches the OTP generated redirect the user to the Main Page 
+                /*if the OTP entered by the user in the textbox matches the OTP generated redirect the user to the Main Page */
                 status_otp.Text = "Otp match";
                 sql_operation();
                 Response.Redirect("MainPage.aspx");
             }
             else
             {
-                //else display that the entered OTP mismatches and user needs to re-enter it
+                /* else display that the entered OTP mismatches and user needs to re-enter it*/
                 status_otp.Text = "Otp mismatch";
             }
         }
-
+        
+        /* 
+        *   This function will perform the sql operations required for storing the user information in the database
+        */
         protected void sql_operation()
         {
-            //Make a SQL Connection to the database
+            /*Make a SQL Connection to the database*/
             SqlConnection con = new SqlConnection("Server=199.79.62.22;uid=training;pwd=Training@786;database=cmp");
 
             try
             {
-                //insert the username, password, email and the secret key generated into the database
+                /* Insert the username, password, email and the secret key generated into the database */
                 SqlCommand cmd = new SqlCommand("insert into cloudlogin values(@username,@password,@email,@secretkey)", con);
 
                 cmd.Parameters.AddWithValue("@username", Session["username"].ToString());
@@ -70,7 +84,7 @@ namespace CloudStorage
                 con.Open();
                 cmd.ExecuteNonQuery();
                 con.Close();
-                //Insert into the databse the username and value "no" indicating that the user has not yet turned on Face Recognition for this account
+                /* Insert into the databse the username and value "no" indicating that the user has not yet turned on Face Recognition for this account */
                 SqlCommand cmd1 = new SqlCommand("insert into Cloudlogin_2FactorAuth values(@username,'no')", con);
                 cmd1.Parameters.AddWithValue("@username", Session["username"].ToString());
 
@@ -78,12 +92,12 @@ namespace CloudStorage
                 cmd1.ExecuteNonQuery();
                 con.Close();
 
-                //Create a table for the user which contains:
-                //username- the name of the user with which he/she signed up
-                //filetype- the file type that the user has stored in his storage,
-                //filepath- the path where the file is stored in the cloud storage,
-                //fileowner- Owner of the file can be the user or the another user who shares his file with the user
-
+                /* Create a table for the user which contains:
+                * username- the name of the user with which he/she signed up
+                * filetype- the file type that the user has stored in his storage,
+                * filepath- the path where the file is stored in the cloud storage,
+                * fileowner- Owner of the file can be the user or the another user who shares his file with the user
+                */
                 SqlCommand cmd2 = new SqlCommand("create table " + Session["username"].ToString() + "_shared (username varchar(50),filetype varchar(10) ,filepath varchar(1000) ,fileowner varchar(50) )", con);
 
                 con.Open();
