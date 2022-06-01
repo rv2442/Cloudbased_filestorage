c69711a493b16ed8b2f25163323a407246b07bee
VineetDabholkar2002 <93699671+VineetDabholkar2002@users.noreply.github.com>
2022-06-01 17:03:54 +0530

    Added comments to CheckOTP.aspx.cs

--- Scloud_Final/CloudStorage/CheckOTP.aspx.cs
+++ Scloud_Final/CloudStorage/CheckOTP.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,14 +12,16 @@ namespace CloudStorage
 {
     public partial class WebForm3 : System.Web.UI.Page
     {
-
+        //Assign an empty string otp to store the OTP (One Time Password)
         string otp = "";
   
         protected void Page_Load(object sender, EventArgs e)
         {
-            //otp = Request.QueryString["name"].ToString();
+            
+            //Session state is an ASP.NET Core scenario for storage of user data while the user browses a web app
             if (Session["username"] != null)
             {
+                //Store session's OTP in string "otp" 
                 otp = Session["OTP"].ToString();
             }
             else
@@ -28,6 +30,7 @@ namespace CloudStorage
             }
             if (!IsPostBack)
             {
+                //Send an alert when the OTP is sent to the Email ID provided by the user
                 ClientScript.RegisterStartupScript(GetType(), "alert", "alert('OTP sent to the given Email ID.');", true);
 
             }
@@ -37,23 +40,26 @@ namespace CloudStorage
         {
             if (otp == txtOTP.Text)
             {
-
+                //if the OTP entered by the user in the textbox matches the OTP generated redirect the user to the Main Page 
                 status_otp.Text = "Otp match";
                 sql_operation();
                 Response.Redirect("MainPage.aspx");
             }
             else
             {
+                //else display that the entered OTP mismatches and user needs to re-enter it
                 status_otp.Text = "Otp mismatch";
             }
         }
 
         protected void sql_operation()
         {
+            //Make a SQL Connection to the database
             SqlConnection con = new SqlConnection("Server=199.79.62.22;uid=training;pwd=Training@786;database=cmp");
 
             try
             {
+                //insert the username, password, email and the secret key generated into the database
                 SqlCommand cmd = new SqlCommand("insert into cloudlogin values(@username,@password,@email,@secretkey)", con);
 
                 cmd.Parameters.AddWithValue("@username", Session["username"].ToString());
@@ -64,7 +70,7 @@ namespace CloudStorage
                 con.Open();
                 cmd.ExecuteNonQuery();
                 con.Close();
-
+                //Insert into the databse the username and value "no" indicating that the user has not yet turned on Face Recognition for this account
                 SqlCommand cmd1 = new SqlCommand("insert into Cloudlogin_2FactorAuth values(@username,'no')", con);
                 cmd1.Parameters.AddWithValue("@username", Session["username"].ToString());
 
@@ -72,23 +78,18 @@ namespace CloudStorage
                 cmd1.ExecuteNonQuery();
                 con.Close();
 
+                //Create a table for the user which contains:
+                //username- the name of the user with which he/she signed up
+                //filetype- the file type that the user has stored in his storage,
+                //filepath- the path where the file is stored in the cloud storage,
+                //fileowner- Owner of the file can be the user or the another user who shares his file with the user
+
                 SqlCommand cmd2 = new SqlCommand("create table " + Session["username"].ToString() + "_shared (username varchar(50),filetype varchar(10) ,filepath varchar(1000) ,fileowner varchar(50) )", con);
 
                 con.Open();
                 cmd2.ExecuteNonQuery();
                 con.Close();
 
-                //SqlCommand cmd3 = new SqlCommand("create table " + Session["username"].ToString() + " (username varchar(25),filepath varchar(1000),userfile varchar(100),sizefile varchar(255))", con);
-
-
-                //con.Open();
-                //cmd3.ExecuteNonQuery();
-                //con.Close();
-
-                //string directoryPath = Server.MapPath("~/MyUploads/" + Session["username"].ToString().Trim()) ;
-
-                //Directory.CreateDirectory(directoryPath);
-                
 
             }
             catch
@@ -96,8 +97,6 @@ namespace CloudStorage
 
                 con.Close();
             }
-        
-
 
         }
     }
