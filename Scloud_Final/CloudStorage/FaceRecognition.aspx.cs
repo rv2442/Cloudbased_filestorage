91632a48d728e4cb0be0cbbe291b733434586fdd
Rahul Vijan <69571769+rv2442@users.noreply.github.com>
2022-06-04 14:19:56 +0530

    Adding Comments

--- Scloud_Final/CloudStorage/FaceRecognition.aspx.cs
+++ Scloud_Final/CloudStorage/FaceRecognition.aspx.cs
@@ -18,19 +18,22 @@ namespace CloudStorage
 {
     public partial class FaceRecognition : System.Web.UI.Page
     {
+        /* Global Variables */
         int count;
         static string data;
         static string data2;
+        
         protected void Page_Load(object sender, EventArgs e)
         {
-            Response.Write(data);
-            Response.Write(data2);
             if (!IsPostBack)
             {
-                count = 0;
+                count = 0; /* Set Counter to 0 on when Loading Page for the First time */
+                Session["TempUsername"] = Session["username"] /* On 1st Page Load Event save username in Temp session  */
+                Session["username"].Abandon()/* Freeing Resources */
             }
+            
             /* If the username is not defined yet due to some reason return to Login Page */
-            if (Session["username"] == null || Session["username"].ToString() == "")
+            if (Session["TempUsername"] == null || Session["TempUsername"].ToString() == "")
             {
                 Response.Redirect("Login.aspx");
             }
@@ -42,57 +45,83 @@ namespace CloudStorage
             if (count >= 3)
             {
                 Response.Write("<script type = 'text/javascript'> function attempts_exceeded() { alert('3 attempts exceeded please provide key and new password to disable face auth'); redirect() }function redirect() { window.location.href = 'ChangePassword.aspx'; }attempts_exceeded();</script>");
-
+                /* 
+                    <script type = 'text/javascript'>
+                    function attempts_exceeded() {
+                        alert('3 attempts exceeded please provide key and new password to disable face auth'); 
+                        redirect() 
+                    }
+                    function redirect() {
+                        window.location.href = 'ChangePassword.aspx';
+                    }
+                    attempts_exceeded();
+                    </script>
+                *
+                * Above Function is used to redirect user to Reset Password Page when there are 3 or more Failed Face Recognitions
+                */
             }
         }
 
+        /* 
+        * Pre-requisite
+        * (This Function is Used when a button is clicked on the page to capture the face,
+        * When that button is clicked it activates a function in JS called abcd() which captures the image and saves it as a base64 string
+        * This string is saved in a hidden field 
+        * abcd() clicks a hidden button  which activates this function) 
+        *
+        * This Function extracts that base64 string from clients browser (Html) to Server (C#)
+        * String is built back to image and Face Recognition Operation is done via a Python Script
+        * If face is recognized then User is redirected to MainPage (Dashboard) 
+        */
         protected void hiddenbtn_Click(object sender, EventArgs e)
         {
 
-            var ab = hidden_img.Value;
+            var base64imgstring = hidden_img.Value; /* extracts base64 string from Hiddden Field */
 
-            string filePath = "C:\\inetpub\\wwwroot\\SCloud\\CloudStorage\\face_rec\\test.jpg";
-            File.WriteAllBytes(filePath, Convert.FromBase64String(ab));
-            var stdOutput = new StringBuilder();
+            string filePath = "C:\\inetpub\\wwwroot\\SCloud\\CloudStorage\\face_rec\\test.jpg";  /* Path where Image which is to be recognized is Saved */
+            File.WriteAllBytes(filePath, Convert.FromBase64String(base64imgstring)); /* Converting Base64 string to Image */
+            var stdOutput = new StringBuilder(); /* Object to save string got from Command line */
             var stderror = new StringBuilder();
 
-            try
+            try /* To Avoid Stopping of App */
             {
-                var p = new Process();
-                p.StartInfo.FileName = "cmd.exe";
-                p.StartInfo.Arguments = @"/k cd /d C:\\inetpub\\wwwroot\\SCloud\\CloudStorage\\face_rec && python face_rec.py ";
-                p.StartInfo.CreateNoWindow = true;
+                var p = new Process(); /* Object to access executables */
+                p.StartInfo.FileName = "cmd.exe"; /* Executable which wer are going to access */
+                p.StartInfo.Arguments = @"/k cd /d C:\\inetpub\\wwwroot\\SCloud\\CloudStorage\\face_rec && python face_rec.py "; /* Command to Execute given as an argument */
+                p.StartInfo.CreateNoWindow = true; /* Do not open CL Interface */ 
                 p.StartInfo.RedirectStandardError = true;
                 p.StartInfo.RedirectStandardOutput = true;
                 p.StartInfo.RedirectStandardInput = false;
                 p.StartInfo.UseShellExecute = false;
-                p.OutputDataReceived += (a, b) => stdOutput.AppendLine(b.Data);
-                p.ErrorDataReceived += (a, b) => stderror.AppendLine(b.Data);
-                p.Start();
-                p.BeginErrorReadLine();
-                p.BeginOutputReadLine();
-                p.WaitForExit(15000);
-                p.Kill();
+                p.OutputDataReceived += (a, b) => stdOutput.AppendLine(b.Data); /* Saves stdoutput */
+                p.ErrorDataReceived += (a, b) => stderror.AppendLine(b.Data); /* Saves stderror log */
+                p.Start();                                                      /* Executes command in Command Line */
+                p.BeginErrorReadLine();                                         /* Begin reading error from CL */
+                p.BeginOutputReadLine();                                        /* Begin reading Output from CL */
+                p.WaitForExit(15000);                                           /* Wait time in ms uptill where we have to read */   
+                p.Kill();                                                       /* Kill Process */
 
-                data = stderror.ToString();
-                data2 = "brr " + stdOutput.ToString() + " brr";
+                data = stderror.ToString(); /* String Saves log of error in case any occur (for debugging) */
+                data2 = stdOutput.ToString(); /* String Saves Output of Face Recognition which is a string with Username saved during Face Registration  (for debugging) */
             }
             catch
             {
-                Response.Write("<br><br> error");
+                Response.Write("<br><br> error"); /* For Debugging */
             }
             finally
             {
-                if (stdOutput.ToString().Trim().ToLower() == Session["username"].ToString().ToLower())
+                /* Check if Facerecognition Output Tag (username) matches current username */
+                if (stdOutput.ToString().Trim().ToLower() == Session["TempUsername"].ToString().ToLower()) 
                 {
-                    Response.Redirect("MainPage.aspx");
+                    Session["username"]=Session["TempUsername"] /* Re-establish User Session */
+                    Session["TempUsername"].Abandon() /* Freeing Resources */
+                    Response.Redirect("MainPage.aspx"); /* Redirect to Dashboard */
 
                 }
                 else
                 {
-                    Response.Write(stdOutput.ToString());
-                    //Response.Write("<script type='text/javascript'>alert('Unknown Face');</script>");
-                    count++;
+                    Response.Write(stdOutput.ToString());  /* Write Invalid User on Top of Page */
+                    count++;                               /* Increment Recognition Fail Counter */
                     
                 }
             }
