14da030553667404a6416ebafc7cd0f7daa8e96c
Vineet Dabholkar <93699671+VineetDabholkar2002@users.noreply.github.com>
2022-07-26 19:44:14 +0530

    Updated comments

--- Scloud_Final/CloudStorage/Settings.aspx.cs
+++ Scloud_Final/CloudStorage/Settings.aspx.cs
@@ -1,4 +1,12 @@
-﻿using System;
+
+/*
+*    @author: Vineet Dabholkar, Rahul Vijan
+*
+*    This Page is used for Settings Page where user can toggle 2F Auth 
+*
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,16 +22,21 @@ namespace CloudStorage
 {
     public partial class Settings : System.Web.UI.Page
     {
-        SqlConnection con = new SqlConnection("Server=199.79.62.22;uid=training;pwd=Training@786;database=cmp");
+        SqlConnection con = new SqlConnection("//Enter your DB credentials//");
 
         public decimal saze;
         bool auth;
+        
+      
         protected void Page_Load(object sender, EventArgs e)
         {
             if (Session["username"] == null)
             {
                 Response.Redirect("Login.aspx");
             }
+            /*
+             *  Display the username, email_id in the settings page
+             */
             if (!IsPostBack)
             {
                
@@ -32,10 +45,15 @@ namespace CloudStorage
                 emailtxt.Text = email;
                 displayusernametxt.Text = usernametxt.Text;
             }
+            
+            /*
+            *   Show the remaining space left in the cloud storage for user (Tier 1 = 1GB). The user can contact the admin to 
+            *   upgrade his/her tier
+            */
             string path = Server.MapPath("~/MyUploads/" + Session["username"].ToString() + "/");
             DirectoryInfo f = new DirectoryInfo(path);
             var saiz = DirSize(f);
-            double temp = Convert.ToInt32(saiz) / 1024 / 1024 / 10.24;
+            double temp = Convert.ToInt32(saiz) / 1024 / 1024 / 10.24; //Convert size to show storage in the progress bar
             saze = decimal.Round(Convert.ToDecimal(temp), 2, MidpointRounding.AwayFromZero);
            // Response.Write(saze);
             auth = sql_check();
@@ -51,6 +69,10 @@ namespace CloudStorage
             }
 
         }
+        
+        /*
+        *   Get the Total Directory Size of the user's storage
+        */
         public static long DirSize(DirectoryInfo d)
         {
             long size = 0;
@@ -71,9 +93,12 @@ namespace CloudStorage
 
         protected void Chart1_Load(object sender, EventArgs e)
         {
-
+                //Empty
         }
-
+           
+        /*
+         *   If the 2F Auth is disabled the change the disable button to enable when clicked
+         */
         protected void disable_button_Click(object sender, EventArgs e)
         {
             SqlCommand cmd = new SqlCommand("update Cloudlogin_2FactorAuth set status_2f='no' where username =@username", con);
@@ -84,7 +109,12 @@ namespace CloudStorage
             disable_button.Visible = false;
             enable_auth.Visible = true;
         }
-
+        
+        
+        /*
+         *  If the 2F Auth is enabled the change the enable button to disable when clicked
+         *  and redirect to Face Register Page
+         */
         protected void enable_auth_Click(object sender, EventArgs e)
         {
             Response.Write("<script type='text/javascript'>function alert_user() { var response = confirm('You are now going to be redirected for registering your face are you sure you wish to enable 2F Auth?'); if (response) { redirect(); } document.getElementById('HiddenField1').value=response; } function redirect() { window.location.href = 'FaceRegister.aspx'; } alert_user();</script>");
@@ -101,7 +131,11 @@ namespace CloudStorage
             }
 
         }
-
+        
+        /*
+        *   Check if the 2F AUth is enabled
+        */
+        
         public bool sql_check()
         {
             SqlCommand cmd = new SqlCommand("select* from Cloudlogin_2FactorAuth where username = @username", con);
@@ -124,7 +158,11 @@ namespace CloudStorage
                 return false;
             }
         }
-
+        
+        /*
+        *   Get the email of the user to display on Settings Page
+        */
+         
         protected string get_email()
         {
             SqlCommand cmd = new SqlCommand("select* from cloudlogin where username = @username", con);
@@ -140,7 +178,11 @@ namespace CloudStorage
             con.Close();
             return email;
         }
-
+        
+        /*
+         * Redirect to the Face Register Page if the user wants to enable 2F Auth for his account
+        */
+        
         protected void redirect()
         {
             Response.Redirect("FaceRegister.aspx");
